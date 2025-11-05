becfb9fc08bba9276373c4dac6f170f00da7b670
nichxlas-bmg <zzxn6tz57x@privaterelay.appleid.com>
2025-11-05 14:32:43 +0100

    1.Hausaufgabe

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -3,8 +3,44 @@ package h1;
 public class H1_main {
 
 	public static void main(String[] args) {
-int i = 1;
-System.out.println(i);
+double guthaben = -100;
+double monEingang = 200;
+int rating = -2;
+boolean warnhinweis = false;
+boolean negativ = false;
+
+
+if (guthaben < 0) {
+	negativ = true;
+	if (monEingang >= guthaben) {
+		rating = rating +1;
+	}
+	else if (monEingang < guthaben) {
+		rating = rating - 1;
+		if(rating < 0) {
+			warnhinweis = true;
+		} else {
+			warnhinweis = false;
+		}
+			
+		
+
+}
+} 
+else if (guthaben > 0){
+    negativ = false;
+    rating = rating + 3;
+    
+}
+else if (guthaben == 0) {
+	negativ = false;
+	rating = rating +2;
+}
+System.out.println(rating);
+System.out.println(warnhinweis);
+System.out.println(negativ);
+
+
 
 
 	}
